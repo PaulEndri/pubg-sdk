972af640208d983fcba0c6eb20ea8439c22d6210
Juan <abrecan.eyndrill@gmail.com>
2018-05-20 21:00:57 -0400

    fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Player from './pubg/models/player';
 import Match from './pubg/models/match';
 import API from './api/PubgApi';
+import Season from './pubg/models/season';
 
 module.exports = {
     Player,
+    Season,
     Match,
     API
 };
